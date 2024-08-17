1188dae98e98f8e991873af31af18f415dcfb59a
Wai-Shing Luk <luk036@gmail.com>
2024-08-17 10:12:56 +0000

    add envconfig.sh

---
+++ envconfig.sh
@@ -0,0 +1,8 @@
+echo Initializing...
+
+eval "$(fzf --bash)"
+
+source /workspace/fzf-git.sh/fzf-git.sh
+
+export FZF_CTRL_T_OPTS="--preview 'bat -n --color=always --line-range :500 {}'"
+export FZF_ALT_C_OPTS="--preview 'lsd --tree --color=always {} | head -200'"
