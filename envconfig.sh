a5ccd601f42383b306eb3ac7fdf24ac0fcc2f221
Wai-Shing Luk <luk036@gmail.com>
2024-09-11 07:59:39 +0000

    updated envcofig.sh

--- envconfig.sh
+++ envconfig.sh
@@ -6,3 +6,4 @@ source /workspace/fzf-git.sh/fzf-git.sh
 
 export FZF_CTRL_T_OPTS="--preview 'bat -n --color=always --line-range :500 {}'"
 export FZF_ALT_C_OPTS="--preview 'lsd --tree --color=always {} | head -200'"
+export HOME="/workspace"
